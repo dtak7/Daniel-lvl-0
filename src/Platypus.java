5b6a236434d9f4a92494848ed366b386d1a02fb9
League <cole.trammer@gmail.com>
2017-05-05 16:58:38 -0700

    drum.aff

---
+++ src/Platypus.java
@@ -0,0 +1,26 @@
+public class Platypus {
+	private String name;
+	void sayBye(){
+		System.out.println("The platypus "+ name + "is more of a ninja than your average platypus");
+	}
+	void sayHi(){
+		System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	}	
+	public static void main(String[] args) {
+		int numbFeet;
+		String name;
+		//1. Make an instance of your new pet platypus
+		Platypus billy = new Platypus ("billy");
+		//2. Call the sayHi method
+		billy.sayHi();
+		//3. Create a constructor in the platypus class so that you can give your platypus a name.
+	}
+		public Platypus(int numbFeet, String name){
+		this.name =name;
+	}
+}
+
+
+
+
+
